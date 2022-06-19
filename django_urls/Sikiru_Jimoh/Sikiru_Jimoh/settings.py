de0d85c0e49e00694dda9c066f11e4bd08ab7a0e
Jasmin1002 <jasmineolofin@gmail.com>
2022-06-19 02:45:55 +0100

    Hide Secret key

--- django_urls/Sikiru_Jimoh/Sikiru_Jimoh/settings.py
+++ django_urls/Sikiru_Jimoh/Sikiru_Jimoh/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialize environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qx4y-fl^t^tqq(f*g2-$wbz3og5c9w)7uvqz(@0sccb4f@w+p+'
+SECRET_KEY = env('SECRET_KEY')
+print('SECRET_KEY: ', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = env('DEBUG')
+print('DEBUG: ', DEBUG)
 
 ALLOWED_HOSTS = []
 
