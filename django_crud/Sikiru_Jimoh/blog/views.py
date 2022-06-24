450aa08d982ba32f05f90ac1d041ceb0f22f223d
Jasmin1002 <jasmineolofin@gmail.com>
2022-06-24 23:10:13 +0100

    Modify models and views commit

--- django_crud/Sikiru_Jimoh/blog/views.py
+++ django_crud/Sikiru_Jimoh/blog/views.py
@@ -1,38 +1,31 @@
-from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
-from django.shortcuts import render
 from .models import Post
-
-class IndexView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all()
-        return context
-
 
 class PostListView(ListView):
     template_name: 'post_list.html'
     model = Post
 
 class PostCreateView(CreateView):
-    model: Post
-    fields: '__all__'
-    success_url: reverse_lazy('blog:all')
+    template_name: 'post_form.html'
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
 
 class PostDetailView(DetailView):
+    template_name: 'post_detail.html'
     model = Post
 
 class PostUpdateView(UpdateView):
-    model: Post
-    fields: '__all__'
-    success_url: reverse_lazy('blog:all')
+    template_name: 'post_form.html'
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
 
 class PostDeleteView(UpdateView):
-    model: Post
-    fields: '__all__'
-    success_url: reverse_lazy('blog:all')
+    template_name: 'post_form.html'
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
